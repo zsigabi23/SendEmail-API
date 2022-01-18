9fa78b43e5d8bb6932dc2a5d499df3eb84e4c2d2
Zolile Sigabi <39824495+zsigabi23@users.noreply.github.com>
2022-01-18 09:11:59 +0200

    Add files via upload

---
+++ SendEmail/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+﻿namespace SendEmail.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SendEmail.Model;
+    using SendEmail.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class EmailController: ControllerBase
+    {
+        private readonly IEmailServices _emailServices;
+
+        public EmailController(IEmailServices emailServices)
+        {
+            _emailServices = emailServices;
+        }
+
+        [HttpPost("SendMail")]
+        public async Task<IActionResult> SendMail([FromForm] EmailReq emailReq)
+        {
+            try
+            {
+                await _emailServices.SendEmailAsync(emailReq);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SendEmail/Model/EmailReq.cs
@@ -0,0 +1,16 @@
+﻿namespace SendEmail.Model
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Collections.Generic;
+
+    public class EmailReq
+    {
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public List<IFormFile> Attachments { get; set; }
+    }
+}
